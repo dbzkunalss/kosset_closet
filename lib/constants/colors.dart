efdb307269bd2e64857b47facd86a5a52b185a4b
rawr <anurag4884@gmail.com>
2020-02-02 15:31:19 +0530

    rewrite initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final kossetDarkPink = Color(0xFFF17791);
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
-
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 import 'package:google_fonts/google_fonts.dart';
 
+import 'screens/splash_screen.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Plantar',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: GoogleFonts.latoTextTheme(
@@ -19,74 +23,7 @@ class MyApp extends StatelessWidget {
         ),
         primarySwatch: Colors.blue,
       ),
-      home: Splash(),
-      
-    );
-  }
-}
-
-class Splash extends StatefulWidget {
-  @override
-  _SplashState createState() => _SplashState();
-}
-
-class _SplashState extends State<Splash> {
-  @override
-  initState() {
-    FirebaseAuth.instance
-        .currentUser()
-        .then((currentUser) => {
-              if (currentUser == null)
-                {
-                  Future.delayed(Duration(seconds: 3))
-                      .then((onValue) =>
-                          Navigator.pushReplacementNamed(context, "/login"))
-                      .catchError((onError) {})
-                }
-              else
-                {
-                  Firestore.instance
-                      .collection("users")
-                      .document(currentUser.uid)
-                      .get()
-                      .then((DocumentSnapshot result) => Future.delayed(
-                              Duration(seconds: 3))
-                          .then((onValue) =>
-                              Navigator.pushReplacementNamed(context, "/task"))
-                          .catchError((onError) {}))
-                      .catchError((err) => print(err))
-                }
-            })
-        .catchError((err) => print(err));
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: blue,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Icon(
-              FontAwesomeIcons.seedling,
-              color: Colors.white,
-              size: 80,
-            ),
-            Text(
-              "Plantar",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w100,
-                  fontSize: 50),
-            ),
-            SizedBox(height: 30),
-            Center(child: CircularProgressIndicator( backgroundColor: Colors.white,))
-          ],
-        ),
-      ),
+      home: SplashScreen(),
     );
   }
 }
