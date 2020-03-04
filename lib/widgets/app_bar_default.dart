565d5fb00971a804b1b15a8c2bd70424ab70921b
rawr <anurag4884@gmail.com>
2020-03-04 10:47:26 +0530

    Tracker screen done

--- lib/widgets/app_bar_default.dart
+++ lib/widgets/app_bar_default.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
 
-AppBar defaultAppBar(){
+AppBar defaultAppBar({TabBar bottom}) {
   return AppBar(
-      backgroundColor: Colors.white,
-      title: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(width: 40),
-          RichText(
-            text: TextSpan(children: [
-              TextSpan(
-                  text: "KOSSET ",
-                  style: TextStyle(color: Color(0xFFF17791), fontSize: 24)),
-              TextSpan(
-                  text: "CLOSET",
-                  style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w800))
-            ]),
-          ),
-          Image.asset(
-            "assets/Group 18.png",
-            scale: 2,
-          )
-        ],
-      ),
-      centerTitle: true,
-    );
+    bottom: bottom,
+    backgroundColor: Colors.white,
+    title: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(width: 40),
+        RichText(
+          text: TextSpan(children: [
+            TextSpan(
+                text: "KOSSET ",
+                style: TextStyle(color: Color(0xFFF17791), fontSize: 24)),
+            TextSpan(
+                text: "CLOSET",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800))
+          ]),
+        ),
+        Image.asset(
+          "assets/Group 18.png",
+          scale: 2,
+        )
+      ],
+    ),
+    centerTitle: true,
+  );
 }
